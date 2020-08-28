795134cbffc38538e78796e860c4910ea24b7766
Nuttapon juabsawat <Nuttaponjuabsawattt@gmail.com>
2020-08-28 06:19:53 +0000

    lab4-4

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
+int main(void)
+{
+  int a, b, c;   // ส่วนประกาศตัวแปร : จองหน่วยความจำไว้เก็บข้อมูล    
+  float ans;     // ส่วนประกาศตัวแปร : จองหน่วยความจำไว้เก็บข้อมูล
+  printf("Enter A : ");
+  scanf("%d", &a);		 // รับค่าที่ต้องการ
+  printf("Enter B : ");
+  scanf("%d", &b);		 // รับค่าที่ต้องการ
+  printf("Enter C : ");
+  scanf("%d", &c);		 // รับค่าที่ต้องการ
+  ans = a + b + c;		 // คำนวณ ตามสูตร
+  printf("Answer is %f", ans); // แสดงผลลัพธ์ที่คำนวณได้
   return 0;
 }
